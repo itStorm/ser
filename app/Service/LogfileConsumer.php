eaa4d44313134a7ab5d035678d8b69fcd5019915
v.krasikov <vl.itstorm@gmail.com>
2016-05-19 14:31:24 +0300

    Process log file (part1) FAM-185

--- app/Service/LogfileConsumer.php
+++ app/Service/LogfileConsumer.php
@@ -3,6 +3,7 @@ namespace Ser\Service;
 
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
 use PhpAmqpLib\Message\AMQPMessage;
+use Symfony\Component\HttpFoundation\File\File;
 
 /**
  * Class LogfileConsumer
@@ -12,8 +13,37 @@ class LogfileConsumer implements ConsumerInterface
 {
     public function execute(AMQPMessage $amqpMessage)
     {
+        $zipArchive = new \ZipArchive();
         $message = unserialize($amqpMessage->getBody());
-        var_dump($message);
-        return 0;
+
+
+        if (!isset($message['path'])) {
+            echo "Bad message: {$amqpMessage->getBody()}\n";
+
+            return self::MSG_REJECT;
+        }
+
+        if ($zipArchive->open($message['path']) !== true) {
+            echo "Bad archive: {$amqpMessage->getBody()}\n";
+
+            return self::MSG_REJECT;
+        }
+
+        if (!($logResource = $zipArchive->getStream('monitor.log'))) {
+            echo "Can't get file handler for monitor.log\n";
+
+            return self::MSG_REJECT;
+        }
+
+        // Read file
+        while (($buffer = fgets($logResource)) !== false) {
+
+            echo 'String: ' . $buffer;
+        }
+
+        fclose($logResource);
+        unlink($message['path']);
+
+        return self::MSG_ACK;
     }
 }
